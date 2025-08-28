c53adfdd91883ff0ac9c0915080eb73e6fa4d6cf
SHAHBOT23 <chuzakichuzi30@gmail.com>
2025-08-29 01:49:54 +0500

    Create Autoreactorr.js

---
+++ Rudra/commands/Autoreactorr.js
@@ -0,0 +1,61 @@
+module.exports.config = {
+  name: "autoreact",
+  version: "1.1.1",
+  hasPermission: 0,
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "Bot React",
+  commandCategory: "No Prefix",
+  usages: '[]',
+  cooldowns: 0,
+};
+const fs = require("fs");
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("Soul") || react.includes("soul")) {
+    var msg = {
+        body: ""
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+          };
+
+    if(react.incluincludes("hyee") || react.includes("fg") || react.includes("e") || react.includes("fg") ||  react.includes("Fg") || react.includes("c") || react.includes("d") || react.includes("") || react.includes("M") || react.includes("b") || react.includes("R") || react.includes("lovely") || react.includes("Love") || react.includes("love") || react.includes("lab") || react.includes("lab") || react.includes("😊") || react.includes("ilove") || react.includes("Ilove") || react.includes("iLove") || react.includes("ilab") || react.includes("iLab") || react.includes("Ilab") || react.includes("q") || react.includes("Laila") || react.includes("kiss") || react.includes("Kiss") || react.includes("y") || react.includes("Krass") || react.includes("k") || react.includes("kras") || react.includes("K") || react.includes("crush") || react.includes("crush") || react.includes("l") || react.includes("kili") || react.includes("fuck") || react.includes("v") || react.includes("g") || react.includes("K") || react.includes("😗") || react.includes("😙") || react.includes("😘") || react.includes("😚") || react.includes("ugh") || react.includes("Ugh") || react.includes("pas") || react.includes("a") || react.includes("h") || react.includes("P") || react.includes("sex") || react.includes("S") || react.includes("☺") || react.includes("porn") || react.includes("Porn") || react.includes("ka") || react.includes("K") || react.includes("In") || react.includes("uff") || react.includes("an") || react.includes("in") || react.includes("cute") || react.includes("Pas") || react.includes("oyet") || react.includes("o") || react.includes("i") || react.includes("iyut") || react.includes("e") || react.includes("Eut") || react.includes("e") || react.includes("😍") || react.includes("s") || react.includes("Shet") || react.includes("send") || react.includes("Send") || react.includes("baby") || react.includes("Baby") || react.includes("babe") || react.includes("Babe") || react.includes("babi") || react.includes("Baby") || react.includes("bby") || react.includes("Bby") || react.includes("kn") || react.includes("t") || react.includes("boy") || react.includes("ach") || react.includes("hye") || react.includes("horn") || react.includes("Horn") || react.includes("ab") || react.includes("no") || react.includes("") || react.includes("l") || react.includes("L") || react.includes("k") || react.includes("m) || react.includes("@bot") || react.includes("n") || react.includes("🤭") || react.includes("🥰") || react.includes("e") || react.includes("P") || react.includes("t") || react.includes("t") || react.includes("py") || react.includes("P") || react.includes("finger") || react.includes("Finger") || react.includes("finger") || react.includes("p") || react.includes("P") || react.includes("er") || react.includes("i") || react.includes("ummah") || react.includes("Mwah") || react.includes("mwuah") || react.includes("Mwuah") || react.includes("angel") || react.includes("Queen") || react.includes("insha") || react.includes("Marry") || react.includes("😇") || react.includes("🤡")) {
+      var lab = {
+        body: ""
+      }
+      api.sendMessage(lab, threadID, messageID);
+    api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
+          };
+    if(react.includes("t") || react.includes("S") || react.includes("ot") || react.includes("et") || react.includes("pair") || react.includes("Pn") || react.includes("Pain") || react.includes("m") || react.includes("Mood off") || react.includes("ayaw ko na") || react.includes("Ayaw ko na") || react.includes("so sad") || react.includes("") || react.includes("n") || react.includes("n") || react.includes("sad") || react.includes("Sad") || react.includes("my gf") || react.includes("sad girl") || react.includes(" 😥") || react.includes("😰") || react.includes("😨") || react.includes("😢") || react.includes(":(") || react.includes("😔") || react.includes("😞") || react.includes("depress") || react.includes("stress") || react.includes("Stress") || react.includes("Depress") || react.includes("depression") || react.includes("Depression") || react.includes("kiya huwa") || react.includes("udas") || react.includes("😭")) {
+      var sad = {
+        body: ""
+      }
+      api.sendMessage(sad, threadID, messageID);
+    api.setMessageReaction("😢", event.messageID, (err) => {}, true)
+          };
+  if(react.includes("baby") || react.includes("cuter") || react.includes("love") || react.includes("hott")) {
+      var emoji = {
+        body: ""
+      }
+      api.sendMessage(flag, threadID, messageID);
+    api.setMessageReaction("❤", event.messageID, (err) => {}, true)
+          };
+  if(react.includes("eve") || react.includes("Eve") || react.includes("morning") || react.includes("Morning") || react.includes("afternoon") || react.includes("Afternoon") || react.includes("evening") || react.includes("eat") || react.includes("Eat") || react.includes("night") || react.includes("nyt") || react.includes("Night") || react.includes("Nyt")) {
+      var heart = {
+        body: ""
+      }
+      api.sendMessage(heart, threadID, messageID);
+    api.setMessageReaction("❤", event.messageID, (err) => {}, true)
+            };
+  if(react.includes("wow") || react.includes("robot")) {
+      var heart = {
+        body: ""
+      }
+      api.sendMessage(heart, threadID, messageID);
+    api.setMessageReaction("😮", event.messageID, (err) => {}, true)
+            }
+        }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+       }
