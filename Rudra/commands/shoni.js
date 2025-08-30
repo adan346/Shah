7dd5ecdd492b90e7590b2bc9bdd7162c0db14fdc
SHAHBOT23 <chuzakichuzi30@gmail.com>
2025-08-30 12:30:14 +0500

    Create shoni.js

---
+++ Rudra/commands/shoni.js
@@ -0,0 +1,39 @@
+module.exports = {
+  config: {
+    name: "baby",
+    aliases: [],
+    author: "Amir",
+    version: "1.0",
+    cooldowns: 5,
+    role: 0,
+    shortDescription: {
+      en: ""
+    },
+    longDescription: {
+      en: "Ai"
+    },
+    category: "Ai",
+    guide: {
+      en: "{p} [text]"
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+  const prompt = args.join(' ');
+
+  try {
+    // Available Models: "v3", "v3-32k", "turbo", "turbo-16k", "gemini"
+    if (!prompt) {
+      api.sendMessage('𝐆 𝐁𝐨𝐋𝐞𝐢𝐧 baby 𝐊𝐢𝐢 𝐉𝐚𝐚𝐍 ◉‿◉', event.threadID, event.messageID);
+      api.setMessageReaction('❓', event.messageID, () => {}, true);
+    } else {
+      api.setMessageReaction('⏱️', event.messageID, () => {}, true);
+      const response = await herc.question({ model: 'v3', content: prompt });
+      api.sendMessage(response.reply, event.threadID, event.messageID);
+      api.setMessageReaction('', event.messageID, () => {}, true);
+    }
+  } catch (error) {
+    api.sendMessage('⚠️ Something went wrong: ' + error, event.threadID, event.messageID);
+    api.setMessageReaction('⚠️', event.messageID, () => {}, true);
+  }
+  }
+};
